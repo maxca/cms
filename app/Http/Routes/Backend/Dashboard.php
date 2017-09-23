9d2e7d2c1d09d90e0a48443a800158a487ca0cf4
Samark Chaisanguan <samarkchsngn@gmail.com>
2017-09-23 18:01:53 +0700

    add simple  side bar

--- app/Http/Routes/Backend/Dashboard.php
+++ app/Http/Routes/Backend/Dashboard.php
@@ -1,3 +1,7 @@
 <?php
 
 Route::get('dashboard', 'DashboardController@index')->name('admin.dashboard');
+Route::group(['prefix' => 'transaction', 'as' => 'transaction.'], function () {
+    Route::get('transaction', 'MainBackendController@index')->name('list');
+
+});
